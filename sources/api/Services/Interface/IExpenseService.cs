3c85981c639bb0b009ff43025650036d9f644eba
baba <93325588+baplec@users.noreply.github.com>
2024-05-04 21:50:42 +0200

    GetExpensesBy : GroupId / UserId / UserIdAndGroupId (#54)

--- sources/api/Services/Interface/IExpenseService.cs
+++ sources/api/Services/Interface/IExpenseService.cs
@@ -9,5 +9,8 @@ namespace DotNetAPI.Services.Interface
         Task<Expense> CreateExpense(Expense expense);
         Task UpdateExpense(Expense expense);
         Task DeleteExpense(int id);
+        Task<IEnumerable<Expense>> GetExpensesByGroupId(int groupId);
+        Task<IEnumerable<Expense>> GetExpensesByUserId(int userId);
+        Task<IEnumerable<Expense>> GetExpensesByUserIdAndGroupId(int userId, int groupId);
     }
 }
